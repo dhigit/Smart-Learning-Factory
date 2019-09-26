22b2fbaa08328cd4c96e8a85fb0ae2760d9ef6dd
Yudhi Paramartha <yudhi.chk@gmail.com>
2019-09-26 09:34:56 +0900

    FINALPROJECT of SLF

---
+++ FINAL PROJECT/Project_3.1/Interface_PLS_PCRegistry/Interface_PLS_PCRegistry/Interface_PLS_PCRegistry/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Interface_PLS_PCRegistry
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.ComOpen = new System.Windows.Forms.Button();
+            this.ComClose = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // ComOpen
+            // 
+            this.ComOpen.Location = new System.Drawing.Point(12, 12);
+            this.ComOpen.Name = "ComOpen";
+            this.ComOpen.Size = new System.Drawing.Size(113, 51);
+            this.ComOpen.TabIndex = 0;
+            this.ComOpen.Text = "Connect";
+            this.ComOpen.UseVisualStyleBackColor = true;
+            this.ComOpen.Click += new System.EventHandler(this.ComOpen_Click);
+            // 
+            // ComClose
+            // 
+            this.ComClose.Enabled = false;
+            this.ComClose.Location = new System.Drawing.Point(140, 12);
+            this.ComClose.Name = "ComClose";
+            this.ComClose.Size = new System.Drawing.Size(113, 51);
+            this.ComClose.TabIndex = 1;
+            this.ComClose.Text = "Disconnect";
+            this.ComClose.UseVisualStyleBackColor = true;
+            this.ComClose.Click += new System.EventHandler(this.ComClose_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(265, 81);
+            this.Controls.Add(this.ComClose);
+            this.Controls.Add(this.ComOpen);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "PLC Registry Control";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ComOpen;
+        private System.Windows.Forms.Button ComClose;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ FINAL PROJECT/Project_3.1/Interface_PLS_PCRegistry/Interface_PLS_PCRegistry/Interface_PLS_PCRegistry/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using ACTMULTILib;
+using Microsoft.Win32;
+
+namespace Interface_PLS_PCRegistry
+{
+    public partial class Form1 : Form
+    {
+        ActEasyIFClass plc = new ActEasyIFClass();
+        RegistryKey plcData_y;
+
+        string[] yAddressName = new string[6] { "Y0", "Y1", "Y2", "Y3", "Y4", "Y5" };
+        int[] yResultData = new int[6];
+
+        public Form1()
+        {
+            InitializeComponent();
+            ComClose.Enabled = false;
+            plcData_y = Registry.CurrentUser.CreateSubKey("PLC_DATA");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            plc.Close();
+        }
+
+        int stationNo = 1;
+        private void ComOpen_Click(object sender, EventArgs e)
+        {
+            plc.ActLogicalStationNumber = 1;
+
+            if (stationNo == 1)
+            {
+                int open = plc.Open();
+                if (open != 0)
+                {
+                    MessageBox.Show("Communication Error!");
+                }
+                else
+                {
+                    MessageBox.Show("Communication Success!");
+                    ComOpen.Enabled = false;
+                    ComClose.Enabled = true;
+
+                    timer1.Start();
+                    this.WindowState = FormWindowState.Minimized;
+                }
+            }
+        }
+
+        private void ComClose_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            plc.Close();
+            MessageBox.Show("Communication is Disconnected!");
+            ComOpen.Enabled = true;
+            ComClose.Enabled = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            for (int i=0; i<yAddressName.Length; i++)
+            {
+                plc.GetDevice(yAddressName[i], out yResultData[i]);
+                plcData_y.SetValue(yAddressName[i], yResultData[i]);
+            }
+        }
+    }
+}
